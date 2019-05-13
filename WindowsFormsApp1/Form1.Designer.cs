3b8f93dcee2fc38b7284727488ea4d737dd5b7c3
Nikita <colmakov.nikita@mail.ru>
2019-05-14 02:27:15 +0300

    created GUI

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericOrganizations = new System.Windows.Forms.NumericUpDown();
+            this.numericYears = new System.Windows.Forms.NumericUpDown();
+            this.numericColumns = new System.Windows.Forms.NumericUpDown();
+            this.ColumnX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericOrganizations)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericYears)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericColumns)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnX});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 199);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(300, 66);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Количество предприятий";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Количество лет плана";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(226, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Количество столбцов(выделяемые суммы)";
+            // 
+            // numericOrganizations
+            // 
+            this.numericOrganizations.Location = new System.Drawing.Point(211, 27);
+            this.numericOrganizations.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericOrganizations.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericOrganizations.Name = "numericOrganizations";
+            this.numericOrganizations.Size = new System.Drawing.Size(60, 20);
+            this.numericOrganizations.TabIndex = 4;
+            this.numericOrganizations.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // numericYears
+            // 
+            this.numericYears.Location = new System.Drawing.Point(211, 56);
+            this.numericYears.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericYears.Name = "numericYears";
+            this.numericYears.Size = new System.Drawing.Size(60, 20);
+            this.numericYears.TabIndex = 5;
+            this.numericYears.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericColumns
+            // 
+            this.numericColumns.Location = new System.Drawing.Point(256, 121);
+            this.numericColumns.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericColumns.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericColumns.Name = "numericColumns";
+            this.numericColumns.Size = new System.Drawing.Size(57, 20);
+            this.numericColumns.TabIndex = 6;
+            this.numericColumns.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // ColumnX
+            // 
+            this.ColumnX.HeaderText = "Xi";
+            this.ColumnX.Name = "ColumnX";
+            this.ColumnX.ReadOnly = true;
+            this.ColumnX.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericColumns);
+            this.Controls.Add(this.numericYears);
+            this.Controls.Add(this.numericOrganizations);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericOrganizations)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericYears)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericColumns)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericOrganizations;
+        private System.Windows.Forms.NumericUpDown numericYears;
+        private System.Windows.Forms.NumericUpDown numericColumns;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnX;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int previousOrganizationsAmount;
+        private int previousYearsAmount;
+        private int previousColumnsAmount;
+
+        public Form1()
+        {            
+            InitializeComponent();
+            InitializeForm();
+            numericOrganizations.ValueChanged += NumericOrganizations_ValueChanged;
+            this.dataGridView1.RowsAdded += new DataGridViewRowsAddedEventHandler(this.DataGridView1_RowsAdded);
+            this.dataGridView1.RowsRemoved += new DataGridViewRowsRemovedEventHandler(DataGridView1_RowsRemoved);
+            this.dataGridView1.ColumnAdded += DataGridView1_ColumnAdded;
+            this.dataGridView1.ColumnRemoved += DataGridView1_ColumnRemoved;
+            numericColumns.ValueChanged += NumericColumns_ValueChanged;
+        }
+
+        private void NumericColumns_ValueChanged(object sender, EventArgs e)
+        {
+            int currentColumnsAmount = Convert.ToInt32(numericColumns.Value);
+            if (previousColumnsAmount < currentColumnsAmount)
+            {
+                dataGridView1.Columns.Add("Column" + currentColumnsAmount, "");
+                previousColumnsAmount++;               
+            }
+            else
+            {
+                previousColumnsAmount--;
+                dataGridView1.Columns.RemoveAt(previousColumnsAmount);                               
+            }
+        }
+
+        private void DataGridView1_ColumnRemoved(object sender, DataGridViewColumnEventArgs e)
+        {
+            ChangeWidth();
+        }
+
+        private void DataGridView1_ColumnAdded(object sender, DataGridViewColumnEventArgs e)
+        {
+            ChangeWidth();
+        }
+
+        private void DataGridView1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            ChangeHeight();
+        }
+
+        private void DataGridView1_RowsRemoved(object sender, DataGridViewRowsRemovedEventArgs e)
+        {
+            ChangeHeight();
+        }
+
+        private void ChangeHeight()
+        {
+            dataGridView1.Height = dataGridView1.Rows.GetRowsHeight(DataGridViewElementStates.Visible);
+        }
+
+        private void ChangeWidth()
+        {
+            dataGridView1.Width = dataGridView1.Columns.GetColumnsWidth(DataGridViewElementStates.Visible)+20;
+        }
+
+        /// <summary>
+        /// Method to add one row if user increase organizations amount, and remove one row
+        /// if user decrease organizations amount
+        /// </summary>        
+        private void NumericOrganizations_ValueChanged(object sender, EventArgs e)
+        {
+            int currentOrganizationsAmount = Convert.ToInt32(numericOrganizations.Value);            
+            if (previousOrganizationsAmount < currentOrganizationsAmount)
+            {           
+                dataGridView1.Rows.Add();                
+                int i = previousOrganizationsAmount++;
+                dataGridView1.Rows[i+1].Cells[0].Value = "f" + (i + 1) + "(X" + (i + 1) + ")";
+            }
+            else
+            {
+                dataGridView1.Rows.Remove(dataGridView1.Rows[dataGridView1.Rows.Count-1] );
+               int i = --previousOrganizationsAmount-1;
+                dataGridView1.Rows[i+1].Cells[0].Value = "f" + (i + 1) + "(X" + (i + 1) + ")";
+            }
+        }        
+
+        private void InitializeForm()
+        {   
+            //initialize default values
+            previousOrganizationsAmount = Convert.ToInt32(numericOrganizations.Value);
+            previousYearsAmount = Convert.ToInt32(numericYears.Value);
+            previousColumnsAmount = Convert.ToInt32(numericColumns.Value);
+
+            //first row reserved to heads of the table
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows[0].Cells[0].Value = "Xi";
+
+            //number of rows depends on default organizations amount
+            for (int i = 0; i < numericOrganizations.Value; i++)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i+1].Cells[0].Value = "f"+(i+1)+"(X"+(i+1)+")";
+            }
+
+            //number of columns depends on default columns amount
+            for (int i = 0; i < numericColumns.Value; i++)
+            {
+                dataGridView1.Columns.Add("Column"+(i+1),"0");
+            }           
+        }
+       
+    }
+}
